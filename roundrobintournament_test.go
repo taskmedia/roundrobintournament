909d6f86814c30dde431ad26d08998c7d3584d87
Marco <marco@task.media>
2021-10-16 00:13:15 +0200

    add go test for even and odd count of teams

---
+++ roundrobintournament_test.go
@@ -0,0 +1,65 @@
+package roundrobintournament
+
+import (
+	"testing"
+)
+
+// testing odd teams number
+func TestGenerateRoundRobinTournamentMatches3Teams(t *testing.T) {
+	teams := []string{
+		"TeamA",
+		"TeamB",
+		"TeamC",
+	}
+
+	matches := GenerateRoundRobinTournamentMatches(teams)
+
+	expectedmatches := [][]string{
+		{"TeamA", "TeamB"},
+		{"TeamA", "TeamC"},
+		{"TeamC", "TeamB"},
+	}
+
+	if !slicesEqual(expectedmatches, matches) {
+		t.Fatalf("Expected matches for a three teams tournament was not satisfied:\nexpected:\n%s\n\ngot:\n%s\n", expectedmatches, matches)
+	}
+}
+
+// testing even teams number
+func TestGenerateRoundRobinTournamentMatches4Teams(t *testing.T) {
+	teams := []string{
+		"TeamA",
+		"TeamB",
+		"TeamC",
+		"TeamD",
+	}
+
+	matches := GenerateRoundRobinTournamentMatches(teams)
+
+	expectedmatches := [][]string{
+		{"TeamA", "TeamB"},
+		{"TeamC", "TeamD"},
+		{"TeamA", "TeamD"},
+		{"TeamB", "TeamC"},
+		{"TeamA", "TeamC"},
+		{"TeamD", "TeamB"},
+	}
+
+	if !slicesEqual(expectedmatches, matches) {
+		t.Fatalf("Expected matches for a four teams tournament was not satisfied:\nexpected:\n%s\n\ngot:\n%s\n", expectedmatches, matches)
+	}
+}
+
+func slicesEqual(a, b [][]string) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i := range a {
+		for k := range a[i] {
+			if a[i][k] != b[i][k] {
+				return false
+			}
+		}
+	}
+	return true
+}
