f0721e987ef0850d131fe2357b7125cef1577dc5
Marco <marco@task.media>
2021-10-28 20:24:04 +0200

    fix calculation of odd teams
    The calculation for a odd number of teams was not correct for more than 3 teams (5,7,...).
    This fixes the algorithm by adding a dummy team (generated uuid) and removing its matches it afterwards.
    
    fixes #4

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/taskmedia/roundrobintournament
 
 go 1.13
+
+require github.com/google/uuid v1.3.0 // indirect
--- roundrobintournament.go
+++ roundrobintournament.go
@@ -1,34 +1,27 @@
 package roundrobintournament
 
+import (
+	"github.com/google/uuid"
+)
+
 // GenerateRoundRobinTournamentMatches generates a 2d slice of matches of a single round robin tournament.
 // Each team will play one time against all other teams.
 func GenerateRoundRobinTournamentMatches(teams []string) [][]string {
 	matches := make([][]string, 0)
 
+	dummy := "even"
+	if len(teams)%2 != 0 {
+		dummy = uuid.New().String()
+		teams = append(teams, dummy)
+	}
+
 	rotation := make([]string, len(teams))
 	copy(rotation, teams)
 
-	var cachedTeam string
-
 	for i := 0; i < (len(teams) - 1); i++ {
 		rotationLen := len(rotation)
-
-		if len(teams)%2 == 0 {
-			// number of teams is even - cachedTeam is not required
-			for i := 0; i < len(rotation); i = i + 2 {
-				matches = append(matches, []string{rotation[i], rotation[i+1]})
-			}
-		} else {
-			// number of teams is odd - cachedTeam is required
-			for i := 0; i < len(rotation)-1; i = i + 2 {
-				matches = append(matches, []string{rotation[i], rotation[i+1]})
-				if cachedTeam != "" {
-					matches = append(matches, []string{cachedTeam, rotation[i+2]})
-					cachedTeam = ""
-				} else {
-					cachedTeam = rotation[i+2]
-				}
-			}
+		for i := 0; i < len(rotation); i = i + 2 {
+			matches = append(matches, []string{rotation[i], rotation[i+1]})
 		}
 
 		// rotate teams for next round
@@ -39,5 +32,27 @@ func GenerateRoundRobinTournamentMatches(teams []string) [][]string {
 		rotation = rotationHelper
 	}
 
+	// remove dummy matches
+	if dummy != "even" {
+		i := 0
+		for _, x := range matches {
+			if !stringSlicecontains(x, dummy) {
+				matches[i] = x
+				i++
+			}
+		}
+		matches = matches[:i]
+	}
+
 	return matches
+}
+
+// Iterate over slice of string  to check whether it an element or not
+func stringSlicecontains(s []string, e string) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
 }
